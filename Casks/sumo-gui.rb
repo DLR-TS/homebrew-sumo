6d709e47d86eceb9bc8ff7e93a42e589160aeac6
Matthias Schwamborn <matthias.schwamborn@dlr.de>
2020-06-02 11:45:27 +0200

    add sumo-gui cask

---
+++ Casks/sumo-gui.rb
@@ -0,0 +1,17 @@
+cask 'sumo-gui' do
+  version :latest
+  sha256 :no_check
+
+  url "https://sumo.dlr.de/daily/sumo-gui-macos-git.zip"
+  name 'sumo-gui-macos'
+  homepage "https://projects.eclipse.org/projects/technology.sumo"
+
+  app 'sumo-git/sumo-gui-macos/sumo-gui.app', target: 'SUMO GUI.app'
+  app 'sumo-git/sumo-gui-macos/netedit.app', target: 'Netedit.app'
+  app 'sumo-git/sumo-gui-macos/OSM Web Wizard.app', target: 'OSM Web Wizard.app'
+
+  caveats do
+    puts "Before running the apps, please verify that your SUMO_HOME environment variable is set correctly.\n"
+    unsigned_accessibility
+  end
+end
