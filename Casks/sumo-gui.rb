865935ca247b85f564700503d288a8a2c9eb9949
Matthias Schwamborn <matthias.schwamborn@dlr.de>
2021-04-14 19:21:19 +0200

    sumo-gui: audit fixes

--- Casks/sumo-gui.rb
+++ Casks/sumo-gui.rb
@@ -3,7 +3,9 @@ cask "sumo-gui" do
   sha256 :no_check
 
   url "https://sumo.dlr.de/daily/sumo-gui-macos-git.zip"
+      verified: "sumo.dlr.de/daily/"
   name "sumo-gui-macos"
+  desc "Simulation of Urban MObility GUI"
   homepage "https://projects.eclipse.org/projects/automotive.sumo"
 
   app "sumo-git/sumo-gui-macos/sumo-gui.app", target: "SUMO GUI.app"
